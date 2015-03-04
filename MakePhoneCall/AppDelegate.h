863d2918c1b617483e9439594ea6d5d3afa4a5fe
LOVE_IOS <shenzhenguo110@163.com>
2015-03-04 09:38:22 +0800

    Initial Commit

---
+++ MakePhoneCall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MakePhoneCall
+//
+//  Created by shuzhenguo on 15-3-4.
+//  Copyright (c) 2015年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MakePhoneCall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MakePhoneCall
+//
+//  Created by shuzhenguo on 15-3-4.
+//  Copyright (c) 2015年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
